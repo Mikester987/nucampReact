1f7c1f184e4188ed62b358d18f2b2704e2996f84
Michael Barba <mabarba1@hotmail.com>
2022-06-20 09:12:43 -0600

    CampsitesDirectoryPage and Campsite Detail

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import CampsitesList from './features/campsites/CampsitesList.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './App.css';
+import CampsitesDirectroryPage from './pages/CampsitesDirectoryPage';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList campsite />
+      <CampsitesDirectroryPage />
       <Footer />
     </div>
   );
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,9 +1,9 @@
-import CAMPSITES from 'shared/';
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () = {
-    return CAMPSITES:[Math.floor(CAMPSITES.length * Math.random())];
+export const selectRandomCampsite = () => {
+    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 };
